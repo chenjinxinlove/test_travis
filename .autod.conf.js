1f0cfd182d2ab75e75ced05d4b8b154a32558eae
macchen <chenjinxinlove@gamil.com>
2017-06-22 23:31:01 +0800

    init

---
+++ .autod.conf.js
@@ -0,0 +1,34 @@
+'ues strict';
+
+module.exports = {
+  write: true,
+  plugin: 'autod-egg',
+  prefix: '^',
+  devprefix: '^',
+  exclude: [
+    'test/fixtures',
+    'examples',
+    'docs',
+    'run',
+  ],
+  devdep: [
+    'autod',
+    'autod-egg',
+    'eslint',
+    'eslint-config-egg',
+    'egg-bin',
+    'egg-doctools',
+    'egg-plugin-puml',
+    'egg-view-nunjucks',
+  ],
+  dep: [
+    '@types/accepts',
+    '@types/koa',
+    '@types/koa-router',
+  ],
+  semver: [
+    'eslint@3',
+    'koa-bodyparser@2',
+  ],
+  test: 'scripts',
+};
